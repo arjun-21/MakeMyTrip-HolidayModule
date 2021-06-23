f7afbaac4403d7a2ca3c5976b379449ad37e63d0
ARJUN PRABHAKARAN <ARJUN PRABHAKARAN@192.168.1.9>
2021-06-23 17:40:24 +0530

    added all codes

--- MakeMyTrip_Holiday/src/test/java/Testsuites/Featured_test.java
+++ MakeMyTrip_Holiday/src/test/java/Testsuites/Featured_test.java
@@ -9,7 +9,7 @@ public class Featured_test {
 	@Test
 	public void featured_testing() throws InterruptedException
 	{
-	Featured f=new Featured();
+	Featured f=new Featured();    //feature-testing
 	f.featuredexpertsrecommended();
 	f.featuredbestsellingholidays();
 	f.featuredshortbreak();
